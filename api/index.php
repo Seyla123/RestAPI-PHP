368c72948e62c40850eadf44b99128b337b27522
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-16 13:26:28 +0700

    feat: use task controller in index

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php 
-    echo "Hello apis"
+   $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+   $parts = explode('/', $path);
+   $resource = $parts[2] ?? null;
+   $id = $parts[3] ?? null;
+   
+   if($resource !== 'task'){
+      http_response_code(404);
+      exit;
+   }
+
+   require dirname(__DIR__) . "/src/TaskController.php";
+   $taskController = new TaskController();
+   $taskController->processRequest($_SERVER['REQUEST_METHOD'], $id);
 ?>
