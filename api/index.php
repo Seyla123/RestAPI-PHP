4aa486646a9a19e8d37730db02e76c53f80d7114
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-16 19:29:14 +0700

    feat: add Error handler

--- api/index.php
+++ api/index.php
@@ -1,9 +1,11 @@
 <?php 
    // strict mode
    declare(strict_types=1);
-
+   // ini_set("display_errors", "On");
+   
    // autoloader
    require dirname(__DIR__) . "/vendor/autoload.php";
+   set_exception_handler("ErrorHandler::handleException");
 
    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
    $parts = explode('/', $path);
---
+++ src/ErrorHandler.php
@@ -0,0 +1,13 @@
+<?php 
+    class ErrorHandler {
+        public static function handleException(Throwable $exception):void 
+        {
+            echo json_encode([
+                "code" => $exception->getCode(),
+                "message" => $exception->getMessage(),
+                "file" => $exception->getFile(),
+                "line" => $exception->getLine()
+            ]);    
+        }
+    }
+?>
