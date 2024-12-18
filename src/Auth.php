4b3f47896a3f38fa39905e230c93c0e3fb20d1a8
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-18 22:01:34 +0700

    feat : get current user id

--- src/Auth.php
+++ src/Auth.php
@@ -1,6 +1,7 @@
 <?php
-class Auth
+class Auth 
 {
+    private int $user_id; 
     public function __construct(private UserGateway $user_gateway)
     {
         
@@ -14,12 +15,19 @@ class Auth
         }
 
         $api_key = $_SERVER['HTTP_X_API_KEY'];
-        if($this->user_gateway->getByAPIKey($api_key) === false) {
+        $user = $this->user_gateway->getByAPIKey($api_key);
+        if($user === false) {
             http_response_code(401);
             echo json_encode(["message" => "Invalid API Key"]);
             exit;
          }
+
+        $this->user_id = $user['id'];
         return true;
+    }
+    public function getUserId(): int
+    {
+        return $this->user_id;
     }
 }
 ?>
