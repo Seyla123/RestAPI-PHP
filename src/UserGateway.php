d3e757537a03b8adbe0fb1046220ae49f44f3af2
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-19 10:25:55 +0700

    feat : Generate an Encoded Access Token Containing the User Details

--- src/UserGateway.php
+++ src/UserGateway.php
@@ -12,5 +12,13 @@
             $stmt->execute();
             return $stmt->fetch(PDO::FETCH_ASSOC);
         }
+        public function getByUsername(string $username):array | false
+        {
+            $sql = "SELECT * FROM user WHERE username = :username";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindValue(':username', $username, PDO::PARAM_STR);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
     }
 ?>
