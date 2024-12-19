b98ef445c8a59286bc17806d829f0f2e262e78ab
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-19 18:00:41 +0700

    feat : Validate the User in the Refresh Token Using the Database

--- api/refresh.php
+++ api/refresh.php
@@ -27,6 +27,17 @@ try {
 }
 
 $user_id = $payload['sub'];
-var_dump($user_id);
 
+// database connection
+$database = new Database($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+$user_gateway = new UserGateway($database);
+$user = $user_gateway->getById($user_id);
+
+if ($user === false) {
+    http_response_code(401);
+    echo json_encode(["message" => "Invalid authentication"]);
+    exit;
+}
+
+var_dump($user);
 ?>
