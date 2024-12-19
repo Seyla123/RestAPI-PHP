103b8b1fc8b203446644fbbfa04437e94c7b00fa
Seav Seyla <Mrrseyla.758@gmail.com>
2024-12-19 15:19:45 +0700

    feat : Add a Method to Decode the Payload from the JWT

--- src/JWTCodec.php
+++ src/JWTCodec.php
@@ -21,10 +21,36 @@
 
             return "$header.$payload.$signature";
         }
+        public function decode(string $token): array 
+        {
+            if(preg_match(
+                "/^(?<header>.+)\.(?<payload>.+)\.(?<signature>.+)$/", 
+                $token, 
+                $matches)!==1){
+                    throw new Exception("Invalid token format");
+                }
+            $signature = hash_hmac(
+                "sha256", 
+                "$matches[header].$matches[payload]", 
+                "4f6c2d3c4b7a1e9f5c3d2e1b0a4f5e9d6c8a3b2e1f4d5c9b3a2e1f8c7d6e5a4b", 
+                true);
+            $signature_from_token = $this->base64urlDecode($matches["signature"]);
+            if(!hash_equals($signature, $signature_from_token)){
+                throw new Exception("Signature does not match");
+            }
 
-        public function base64urlEncode(string $text):string
+            $payload = json_decode($this->base64urlDecode($matches["payload"]), true);
+            return $payload;
+        }
+        private function base64urlEncode(string $text):string
         {
             return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($text));
         }
+
+        private function base64urlDecode(string $text):string
+        {
+            return base64_decode(str_replace(['-', '_'], ['+', '/'], $text));
+        }
+
     }
 ?>
